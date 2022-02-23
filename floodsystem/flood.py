f00d08115673e42ad9066855f0182f5fcc11b025
Yiyang Wang <yw584@cam.ac.uk>
2022-02-23 22:10:16 +0000

    Done Task 2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,3 +1,4 @@
+from sqlalchemy import true
 from floodsystem.station import MonitoringStation
 from floodsystem.utils import sorted_by_key
 from floodsystem.stationdata import update_water_levels
@@ -13,3 +14,20 @@ def stations_level_over_threshold(stations, tol):
 
     result_list = sorted_by_key(result_list,1,True)
     return result_list
+
+
+def stations_highest_rel_level(stations, N):
+    update_water_levels(stations)
+    water_level_list = []
+    for entry in stations:
+        if not entry.relative_water_level():
+            pass
+        else:
+            water_level_list.append((entry.name, entry.relative_water_level()))
+    water_level_list = sorted_by_key(water_level_list,1,True)
+
+    return_list = water_level_list[0:N]
+    return return_list
+
+
+    
